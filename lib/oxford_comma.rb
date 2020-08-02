649956ce803918f024f94fc8bb57019f929c4d59
Jonathan Snipe <jon.mike919@gmail.com>
2020-08-02 11:10:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+ if array.length == 1 
+   return "kiwi"  
+elsif array.length == 2
+    return ("kiwi and durian")
+  elsif array.length == 3
+    return ("kiwi, durian, and starfruit")
+  elsif array.length > 3
+    return ("kiwi, durian, starfruit, mangos, and dragon fruits,").split(",")
+  end
 end
+
